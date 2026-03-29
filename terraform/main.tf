4f04f30d51eaba586509fbb137017929b7d492fa
Clichy <clichylearns@gmail.com>
2026-03-29 14:40:33 -0700

    fix: remediate Checkov findings, add suppressions for Basic SKU limitations

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,6 +32,12 @@ resource "azurerm_container_registry" "main" {
   sku                 = var.acr_sku
   admin_enabled       = false
 
+
+  trust_policy {
+    enabled = false
+  }
+
+
   tags = azurerm_resource_group.main.tags
 }
 
@@ -58,8 +64,10 @@ resource "azurerm_key_vault" "main" {
   resource_group_name        = azurerm_resource_group.main.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
-  purge_protection_enabled   = false
+  purge_protection_enabled   = true
   soft_delete_retention_days = 7
+
+  public_network_access_enabled = false
 
   network_acls {
     default_action = "Deny"
